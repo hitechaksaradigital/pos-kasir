2485516aec4fe0817da6b87c1c3489a3919b176d
M. Hamdani Ilham Latjoro <hitechaksaradigital@gmail.com>
2026-06-13 11:09:23 +0800

    Update supabase-schema.sql

--- supabase-schema.sql
+++ supabase-schema.sql
@@ -33,3 +33,13 @@ CREATE POLICY "Allow public update access"
 
 CREATE POLICY "Allow public delete access"
   ON products FOR DELETE TO public USING (true);
+
+INSERT INTO products (name, price, image, category) VALUES
+('Produk 1', 45.00, 'https://images.pexels.com/photos/32885654/pexels-photo-32885654.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'Apparel'),
+('Quantum Run V2', 120.00, 'https://images.pexels.com/photos/2364580/pexels-photo-2364580.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'Footwear'),
+('Heritage Denim Jacket', 89.99, 'https://images.pexels.com/photos/18533669/pexels-photo-18533669.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'Apparel'),
+('Precision Chronograph', 250.00, 'https://images.pexels.com/photos/9261531/pexels-photo-9261531.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'Accessories'),
+('Merino Wool Beanie', 35.00, 'https://images.pexels.com/photos/8219320/pexels-photo-8219320.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'Accessories'),
+('Ultra Boost Sneakers', 180.00, 'https://images.pexels.com/photos/2364580/pexels-photo-2364580.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'Footwear'),
+('Wireless Earbuds Pro', 149.99, 'https://images.pexels.com/photos/9261531/pexels-photo-9261531.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'Electronics'),
+('Smart Fitness Band', 79.99, 'https://images.pexels.com/photos/9261531/pexels-photo-9261531.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'Electronics');
